682d43efd66520075ff96c834f5f89e59b97be92
Alessandro Paperini <alessandro.paperini@drwolf.it>
2019-10-02 16:40:23 +0200

    :wrench: use queue name from properties

--- src/main/java/it/laterale/cloud/config/RabbitMQConfig.java
+++ src/main/java/it/laterale/cloud/config/RabbitMQConfig.java
@@ -1,15 +1,27 @@
 package it.laterale.cloud.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
+@Slf4j
 public class RabbitMQConfig {
+
+    @Autowired
+    Environment env;
 
     @Bean
     public Queue ballQueue() {
-        return new Queue("ball-queue");
+        return createQueue(this.env.getProperty("spring.rabbitmq.queue.ball.name"));
+    }
+
+    private Queue createQueue(String name) {
+        log.debug("Creating queue {}", name);
+        return new Queue(name);
     }
 
 }
