a0f74eeed2d521c8866ce5e2be003be7768f6d59
Alessandro Paperini <alessandro.paperini@drwolf.it>
2019-10-02 11:55:07 +0200

    :sparkles: added ball input API

---
+++ src/main/java/it/laterale/cloud/controllers/BallController.java
@@ -0,0 +1,28 @@
+package it.laterale.cloud.controllers;
+
+import it.laterale.cloud.dtos.Ball;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/ball")
+public class BallController {
+
+    @Autowired
+    private Queue ballQueue;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @PostMapping
+    public ResponseEntity<Void> input(@RequestBody Ball body) {
+        rabbitTemplate.convertAndSend(ballQueue.getName(), body);
+        return ResponseEntity.noContent().build();
+    }
+}
