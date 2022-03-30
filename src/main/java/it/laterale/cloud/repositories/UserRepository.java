7d10fe930fdd88f27db36736ed1a26c62b983b69
Alessandro Paperini <alessandro.paperini@drwolf.it>
2022-03-30 12:53:33 +0200

    :construction: removed unnecessary code

--- src/main/java/it/laterale/cloud/repositories/UserRepository.java
+++ src/main/java/it/laterale/cloud/repositories/UserRepository.java
@@ -4,17 +4,10 @@ import it.laterale.cloud.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
-
-    Collection<User> findByOrderByAgeAsc();
-
-    Collection<User> findByAgeGreaterThan(Integer age);
-
-    Collection<User> findByName(String name);
 }
